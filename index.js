63924bdd35b3336945c0fbf56f46f3208655efb8
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-07-29 15:27:27 -0300

    fix port

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 
-const PORT = process.env.PORT || 5000;
+const PORT =  process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/test", function roureHandler(req, res) {
-    res.send('ok');
+app.use("/", (req, res) => {
+    res.status(200).send('OK');
 })
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
